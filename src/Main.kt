fb8664d3a2643a5a83e984ad088c1ef23c679e0f
Admin <reteamboss@gmail.com>
2024-09-01 14:48:53 +0300

    Задание 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,28 @@
 fun main() {
-    println("Hello World!")
+    val printerOne = object : Printer("HP", "White",300) {
+        override fun printerPage() {
+            println("Принтер может напечатать $pages страниц")
+        }
+
+        override fun printInfo() {
+            println("Принтер $name цвет $color запас печати $pages страниц")
+        }
+    }
+
+    val printerTwo = object : Printer("Samsung", "Black",500) {
+        override fun printerPage() {
+            println("Принтер может напечатать $pages страниц")
+        }
+
+        override fun printInfo() {
+            println("Принтер $name цвет $color запас печати $pages страниц")
+        }
+    }
+
+    printerOne.printerPage()
+    printerOne.printInfo()
+
+    println("------------------------------")
+    printerTwo.printerPage()
+    printerTwo.printInfo()
 }
--- src/Printer.kt
+++ src/Printer.kt
@@ -1,2 +1,6 @@
-class Printer {
+abstract class Printer(val name: String = "", val color: String = "", val pages: Int = 0) {
+
+
+    abstract fun printerPage()
+    abstract fun printInfo()
 }
